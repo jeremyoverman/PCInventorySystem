ba8f3ce520a8c0f65d6851ca99fa8fb8cb815840
jeremy <jeremy@JeremysPC>
2015-05-18 21:34:32 -0400

    Added menubar, toolbar, and properties panel. No functionality yet.

--- viewer.py
+++ viewer.py
@@ -7,6 +7,48 @@ Created on May 11, 2015
 import wx, wx.grid
 import database
 
+class MenuBar(wx.MenuBar):
+    def __init__(self, *args, **kwargs):
+        wx.MenuBar.__init__(self, *args, **kwargs)
+        
+        self.file_menu = wx.Menu()
+        file_exit = self.file_menu.Append(wx.ID_EXIT, "E&xit", "Close the application.")
+        self.Append(self.file_menu, "&File")
+        
+        self.filter_menu = wx.Menu()
+        self.Append(self.filter_menu, "Fi&lters")
+
+class Icons():
+    def __init__(self):
+        self.ADD = wx.Bitmap("icons/add.png")
+        self.REMOVE = wx.Bitmap("icons/remove.png")
+
+class ToolBar(wx.ToolBar):
+    def __init__(self, *args, **kwargs):
+        wx.ToolBar.__init__(self, *args, **kwargs)
+        
+        self.AddTool(-1, icons.ADD)
+        self.AddTool(-1, icons.REMOVE)
+        
+        self.Realize()
+
+class NavTree(wx.TreeCtrl):
+    def __init__(self, *args, **kwargs):
+        wx.TreeCtrl.__init__(self, *args, **kwargs)
+        
+        self.root = self.AddRoot("Navigation")
+        
+class NavPanel(wx.Panel):
+    def __init__(self, *args, **kwargs):
+        wx.Panel.__init__(self, *args, **kwargs)
+        
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.nav_tree = NavTree(self, size=(150,1))
+        
+        self.main_sizer.Add(self.nav_tree, 1, wx.EXPAND|wx.BOTH)
+        
+        self.SetSizerAndFit(self.main_sizer)
+
 class Grid(wx.grid.Grid):
     def __init__(self, *args, **kwargs):
         wx.grid.Grid.__init__(self, *args, **kwargs)
@@ -36,7 +78,7 @@ class Grid(wx.grid.Grid):
             self.SetCellValue(row, col, value)
             col += 1
 
-class MainPanel(wx.Panel):
+class GridPanel(wx.Panel):
     def __init__(self, *args, **kwargs):
         wx.Panel.__init__(self, *args, **kwargs)
         
@@ -47,14 +89,46 @@ class MainPanel(wx.Panel):
         self.main_sizer.Add(self.main_grid, 1)
         
         self.SetSizerAndFit(self.main_sizer)
+        
+        self.Layout()
+
+class PropertiesPanel(wx.Panel):
+    def __init__(self, *args, **kwargs):
+        wx.Panel.__init__(self, *args, **kwargs)
+        
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        self.SetSizer(self.main_sizer)
+        self.Layout()
+
+class MainPanel(wx.Panel):
+    def __init__(self, *args, **kwargs):
+        wx.Panel.__init__(self, *args, **kwargs)
+        
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.top_hor_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.nav_panel = NavPanel(self)
+        self.grid_panel = GridPanel(self)
+        self.properties_panel = PropertiesPanel(self, size=(200, 200))
+        
+        self.main_sizer.Add(self.top_hor_sizer, 1, wx.EXPAND|wx.BOTH)
+        self.top_hor_sizer.Add(self.nav_panel, 0, wx.EXPAND|wx.VERTICAL|wx.RIGHT, 5)
+        self.top_hor_sizer.Add(self.grid_panel, 1, wx.EXPAND|wx.BOTH)
+        self.main_sizer.Add(self.properties_panel, 0, wx.EXPAND|wx.BOTH)
+        
+        self.SetSizerAndFit(self.main_sizer)
         self.Layout()
 
 class GUI(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
         
+        self.menubar = MenuBar()
+        self.toolbar = ToolBar(self)
         self.main_panel = MainPanel(self)
         
+        self.SetToolBar(self.toolbar)
+        self.SetMenuBar(self.menubar)
         self.Show()
 
 def getItemList():
@@ -70,12 +144,13 @@ def getItemList():
 if __name__ == '__main__':
     app = wx.App()
     
-    gui = GUI(None)
+    icons = Icons()
+    
+    gui = GUI(None, size=(640,480))
     
     items = getItemList()
-    print items
     
-    gui.main_panel.main_grid.populateList(items)
+    gui.main_panel.grid_panel.main_grid.populateList(items)
     
     
     app.MainLoop()
